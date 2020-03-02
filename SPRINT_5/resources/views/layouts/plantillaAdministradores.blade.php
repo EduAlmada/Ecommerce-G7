8aff6231002cb7b4733dcce0c85e6bc63d21c714
EduAlmada <eduardoalmada3@gmail.com>
2020-03-02 15:28:19 -0300

    Plantilla Usuario

--- SPRINT_5/resources/views/layouts/plantillaAdministradores.blade.php
+++ SPRINT_5/resources/views/layouts/plantillaAdministradores.blade.php
@@ -114,35 +114,6 @@
 
 </body>
 
-<!-- INICIO FOOTER -->
-<footer class="page-footer text-center font-small mt-5 bg-dark">
-
-    <div class="pt-4">
-        <a class="btn-outline-light pl-3 pr-3" href="index.php" target="_self" role="button">Home</a>
-        <a class="btn-outline-light pl-3 pr-3" href="index.php" target="_self" role="button">Sobre Nosotros</a>
-        <a class="btn-outline-light pl-3 pr-3" href="perfil.php" target="_self" role="button">Perfil</a>
-        <a class="btn-outline-light pl-3 pr-3" href="faq.php" target="_self" role="button">Preguntas frecuentes</a>
-        <a class="btn-outline-light pl-3 pr-3" href="registro.php" target="_self" role="button">Registrarse</a>
-        <a class="btn-outline-light pl-3 pr-3" href="login.php" target="_self" role="button">Login</a>
-        <a class="btn-outline-light pl-3 pr-3" href="formularioContacto.php" target="_self" role="button">Contacto</a>
-    </div>
-  
-    <div class="p-4">
-        <a href="https://www.facebook.com" target="_blank">
-            <i class="fab fa-facebook-f mr-3"></i>
-        </a>
-        <a href="https://twitter.com" target="_blank">
-            <i class="fab fa-twitter mr-3"></i>
-        </a>
-        <a href="https://www.youtube.com" target="_blank">
-            <i class="fab fa-youtube mr-3"></i>
-        </a>
-        <a href="https://plus.google.com" target="_blank">
-            <i class="fab fa-google-plus-g mr-3"></i>
-        </a>
-    </div>
-
-</footer>
-<!-- FIN FOOTER -->
+{{-- Esta plantilla de Administrador la dejo sin footer --}}
 
 </html>
